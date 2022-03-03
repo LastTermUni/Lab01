fb5a19ffb64754fce330745886f1acef9f768415
Hendrix <huyndg.itdev@gmail.com>
2022-03-03 10:40:39 +0700

    add readme

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <head>
     <meta charset="utf-8">
-    <link href="./style.css" rel="stylesheet"  >
+    <!-- <link href="./style.css" rel="stylesheet"  > -->
     <!-- <link rel="stylesheet" href="css\bootstrap.min.css">
     <script src="js\bootstrap.min.js"></script> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -12,13 +12,13 @@
         <h2>Xếp loại kết quả tuyển sinh</h2>
         <form action="#" method="post">
 
-        <div class="row">
-            <div class="lbtitle">
-                <label>Điểm Toán</label>
-            </div>
-            <div class="libinput">
-                <input type="number" name="toan" value="<?php echo isset($_POST["toan"]) ? $_POST["toan"] : "" ; ?> "/>
-            </div>
+        <div class="row mb-3">
+            <!-- <div class="lbtitle"> -->
+            <label for="inputMathage"  class="col-sm-2 col-form-label">Điểm Toán</label>
+            <!-- </div> -->
+            <!-- <div class="libinput"> -->
+                <input type="number" name="toan" class="form-control" value="<?php echo isset($_POST["toan"]) ? $_POST["toan"] : "" ; ?> "/>
+            <!-- </div> -->
         </div>
 
         <div class="row">
